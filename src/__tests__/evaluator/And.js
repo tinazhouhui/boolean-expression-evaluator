d5204787f4db896dbae59bf355997cd69ae1a1cd
Tina <tina.zhou.hui@gmail.com>
2022-06-08 21:58:09 +0100

    feat: And operator tests

---
+++ src/__tests__/evaluator/And.js
@@ -0,0 +1,18 @@
+import {describe, test} from '@jest/globals';
+import {And} from '../../evaluator/And';
+import {Constant} from '../../evaluator/Constant';
+
+describe('AND operation', () => {
+    const falseConst = new Constant(false);
+    const trueConst = new Constant(true);
+
+    test('Should evaluate AND to falsy if given false and true', () => {
+        const and = new And(falseConst, trueConst)
+        expect(and.evaluate()).toBeFalsy();
+    })
+
+    test('Should evaluate AND to truthy if given true and true', () => {
+        const and = new And(trueConst, trueConst)
+        expect(and.evaluate()).toBeTruthy();
+    })
+})
