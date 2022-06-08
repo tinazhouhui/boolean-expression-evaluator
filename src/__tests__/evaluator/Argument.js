fa1c046d911ac860303c794035d0ff850ded4979
Tina <tina.zhou.hui@gmail.com>
2022-06-08 21:58:54 +0100

    feat: Argument tests

---
+++ src/__tests__/evaluator/Argument.js
@@ -0,0 +1,14 @@
+import {describe, test} from '@jest/globals';
+import {Argument} from '../../evaluator/Argument';
+
+describe('Argument', () => {
+    test('Should evaluate AND to falsy if given false and true', () => {
+        const falseArg = new Argument(false, 'falseArgument')
+        expect(falseArg.evaluate()).toBeFalsy();
+    })
+
+    test('Should evaluate AND to truthy if given true and true', () => {
+        const trueArg = new Argument(true, 'trueArgument')
+        expect(trueArg.evaluate()).toBeTruthy();
+    })
+})
