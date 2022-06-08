52cb9f0e5f15d629ee4e988cbeb0eefc892e522f
Tina <tina.zhou.hui@gmail.com>
2022-06-08 21:45:04 +0100

    feat: Or operator tests

---
+++ src/__tests__/evaluator/Or.js
@@ -0,0 +1,18 @@
+import {describe, test} from '@jest/globals';
+import {Or} from '../../evaluator/Or';
+import {Constant} from '../../evaluator/Constant';
+
+describe('Or operation', () => {
+    const falseConst = new Constant(false);
+    const trueConst = new Constant(true);
+
+    test('Should evaluate OR to truthy if given false and true', () => {
+        const or = new Or(falseConst, trueConst)
+        expect(or.evaluate()).toBeTruthy();
+    })
+
+    test('Should evaluate OR to falsy if given false and false', () => {
+        const or = new Or(falseConst, falseConst)
+        expect(or.evaluate()).toBeFalsy();
+    })
+})
