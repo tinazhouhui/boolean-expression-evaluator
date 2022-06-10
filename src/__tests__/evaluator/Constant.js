881d6ecdd0a934e43c38df5fdb21a9007186ffec
Tina <tina.zhou.hui@gmail.com>
2022-06-10 17:31:04 +0100

    feat: Constant tests

---
+++ src/__tests__/evaluator/Constant.js
@@ -0,0 +1,14 @@
+import {describe, test} from '@jest/globals';
+import {Constant} from '../../evaluator/Constant';
+
+describe('Constant', () => {
+    test('Should evaluate AND to falsy if given false and true', () => {
+        const falseConst = new Constant(false)
+        expect(falseConst.evaluate()).toBeFalsy();
+    })
+
+    test('Should evaluate AND to truthy if given true and true', () => {
+        const trueConst = new Constant(true)
+        expect(trueConst.evaluate()).toBeTruthy();
+    })
+})
