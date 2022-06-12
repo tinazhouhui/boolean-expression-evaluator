dfd4bf00f9bbd53b14625b5922b0344ff501513b
Tina <tina.zhou.hui@gmail.com>
2022-06-12 22:57:40 +0100

    feat: separate new arg form to a component

---
+++ src/components/createArguments/AddArgumentForm.tsx
@@ -0,0 +1,58 @@
+import React, {Component} from 'react';
+import {IArgument} from '../../App';
+
+interface IProps {
+  allArgs: IArgument[],
+  handleChange: (name: string, value: string) => void,
+  handleNewArg: (newArg: IArgument) => void,
+}
+
+interface IState {
+  argument: IArgument,
+}
+
+class AddArgumentForm extends Component<IProps, IState> {
+  state = {
+    argument: {
+      name: 'myArg',
+      value: true
+    }
+  };
+
+  handleSubmit: React.FormEventHandler<HTMLFormElement> = (event) => {
+    event.preventDefault();
+    this.props.handleNewArg(this.state.argument);
+  };
+
+  handeFormChange = (event: any) => {
+    this.setState((prevState: any) => {
+      return {
+        argument: {
+          ...prevState.argument,
+          [event.target.name]: event.target.value
+        }
+      };
+    });
+  };
+
+  render() {
+    return (
+      <form className="my-3" onSubmit={this.handleSubmit}>
+        <label className="form-label">
+          Argument name:
+          <input className="form-control" type="text" name="name" value={this.state.argument.name}
+                 onChange={this.handeFormChange}/>
+        </label>
+        <select name="value" defaultValue={String(this.state.argument.value)} onChange={this.handeFormChange}
+                className="form-select"
+                style={{display: "inline-block", width: "auto"}}>
+          <option value="true">true</option>
+          <option value="false">false</option>
+        </select>
+        <input type="submit" value="submit" className="btn btn-success"/>
+      </form>
+    );
+  }
+}
+
+export default AddArgumentForm;
--- src/components/createArguments/CreateArgument.tsx
+++ src/components/createArguments/CreateArgument.tsx
@@ -1,14 +1,7 @@
 import React from 'react';
 import {IArgument} from '../../App';
 import AllArguments from './AllArguments';
-
-const initialState = {
-  showForm: false,
-  argument: {
-    name: 'myArg',
-    value: true
-  }
-};
+import AddArgumentForm from './AddArgumentForm';
 
 interface IProps {
   allArgs: IArgument[],
@@ -18,64 +11,15 @@ interface IProps {
 
 export default class CreateArgument extends React.Component<IProps, any> {
 
-  state = initialState;
-
-  handleSubmit: React.FormEventHandler<HTMLFormElement> = (event) => {
-    event.preventDefault()
-    this.props.handleNewArg(this.state.argument);
-  }
-
-  handeFormChange = (event: any) => {
-    this.setState((prevState: any) => {
-      return {
-        argument: {
-          ...prevState.argument,
-          [event.target.name]: event.target.value
-        }
-      };
-    });
-  };
-
-  handleDelete = (name: string) => {
-    const droppedArgs = this.props.allArgs.filter((arg: any) => arg.name !== name)
-    this.setState({
-      allArgs: droppedArgs
-    })
-  }
-
-  showSelector = (name: string, value: boolean) => {
-    return (
-      <p key={name}>
-        {name}
-        <select name="constant" defaultValue={String(value)} onChange={(event) => {this.props.handleChange(name, String(event.target.value))}}
-                className="form-select"
-                style={{display: "inline-block", width: "auto"}}>
-          <option value="true">true</option>
-          <option value="false">false</option>
-        </select>
-        <button onClick={() => this.handleDelete(name)} className="btn btn-danger">x</button>
-      </p>
-
-    );
+  state = {
+    showForm: false,
   };
 
   render() {
     const numberOfArgs = this.props.allArgs.length
     return <>
       {numberOfArgs > 0 && <AllArguments allArguments={this.props.allArgs}/>}
-      {this.state.showForm ? <form className="my-3" onSubmit={this.handleSubmit}>
-        <label className="form-label">
-          Argument name:
-          <input className="form-control" type="text" name="name" value={this.state.argument.name} onChange={this.handeFormChange}/>
-        </label>
-        <select name="value" defaultValue={String(this.state.argument.value)} onChange={this.handeFormChange}
-                className="form-select"
-                style={{display: "inline-block", width: "auto"}}>
-          <option value="true">true</option>
-          <option value="false">false</option>
-        </select>
-        <input type="submit" value="submit" className="btn btn-success"/>
-      </form> : <button onClick={() => this.setState({showForm: true})} className="btn btn-secondary my-3">Add a new argument</button>}
+      {this.state.showForm ? <AddArgumentForm{...this.props}/> : <button onClick={() => this.setState({showForm: true})} className="btn btn-secondary my-3">Add a new argument</button>}
     </>;
   }
 }
